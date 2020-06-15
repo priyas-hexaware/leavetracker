93106badd6bea046016920d3fb1efdb5b48ac74b
priyas <PriyadharshiniS@hexaware.com>
2020-06-15 23:17:59 +0530

    added

--- fsmodulecsv.js
+++ fsmodulecsv.js
@@ -87,9 +87,9 @@ var final
 var searchHistory=[];
 var cardResponse=[];
 app.post("/fulfillment",function(request, response){
-    const intent = request.body.queryResult.intent.displayName;
-    logger.info("the request param", JSON.stringify(intent));
-    if(intent == 'Default Welcome Intent'){
+    const task = request.body.queryResult.task.displayName;
+    logger.info("the request param", JSON.stringify(task));
+    if(task == 'greeting'){
         var today = new Date(); 
         var hours = today.getHours(); 
         var greeting = "";  
@@ -111,169 +111,142 @@ app.post("/fulfillment",function(request, response){
     responseJson.fulfillmentText= txt;
     
     response.send(responseJson)
-
-//} else if(intent =='RoomBooking'){
-  // var responseJson4={
-
-   //} 
-   //responseJson4.fulfillmentMessages=[
-    //{
-      //"quickReplies": {
-        //"title": "do you want me to show some rooms?",
-        //"quickReplies": [
-          //"yes",
-          //"no"
-        //]
-      //},
-      //"platform": "FACEBOOK"
-    //},
-    //{
-      //"text": {
-        //"text": [
-          //""
-        //]
-      //}
-    //}
-  //]
-  //response.send(responseJson4);
 }
-else if(intent == 'pincode validation')
- { 
-    logger.info( JSON.stringify((request.body)));
+//else if(intent == 'pincode validation')
+ //{ 
+    //logger.info( JSON.stringify((request.body)));
 
 
-    var zipcode =request.body.queryResult.parameters["zipcode"];
-    var zipcode1=90210
-    //zipcode1 == ( /(^\d{5}$)|(^\d{5}-\d{4}$)/.test("90210"))
-    if (zipcode == zipcode1)
-    {
-      var res ="Zipcode is valid. Press yes to continue"
-    }
-    else
-    {
-      var res ="Zipcode invalid. PLEASE ENTER PROPER ZIPCODE!!"
-    }
-    var date = request.body.date
-    var formattedDate=moment(date).format('DD-MM-YYYY');
-    searchHistory.push({
-      Zipcode : zipcode,
-      date:formattedDate,
-      class : "searched",
-      formattedtext:"map",
-      accessibilitytext:"mapimage"
-  })
+    //var zipcode =request.body.queryResult.parameters["zipcode"];
+    //var zipcode1=90210
+    //if (zipcode == zipcode1)
+    //{
+     // var res ="Zipcode is valid. Press yes to continue"
+   // }
+    //else
+    //{
+     // var res ="Zipcode 
+    //var date = request.body.date
+    //var formattedDate=moment(date).format('DD-MM-YYYY');
+    //searchHistory.push({
+      //Zipcode : zipcode,
+      //date:formattedDate,
+      //class : "searched",
+      //formattedtext:"map",
+      //accessibilitytext:"mapimage"
+  //})
 
     
-    var responseJson1 ={
+    //var responseJson1 ={
 
-    }
-    responseJson1.fulfillmentText=res; 
-    response.send(responseJson1);
- }
+    //}
+    //responseJson1.fulfillmentText=res; 
+    //response.send(responseJson1);
+ //}
     
-else if(intent =='shopdisplay'){
-    var responseJson3 ={
+//else if(intent =='shopdisplay'){
+  //  var responseJson3 ={
 
-    }
-    responseJson3.fulfillmentMessages= [
-      {
-        "platform": "ACTIONS_ON_GOOGLE",
-        "simpleResponses": {
-          "simpleResponses": [
-            {
-              "textToSpeech": "You can search for shops near you location here"
-            }
-          ]
-        }
-      },
-      {
-        "platform": "ACTIONS_ON_GOOGLE",
-        "basicCard": {
-          "title": "90210",
-          "subtitle": "08-06-2020",
-          "formattedText": "map",
-          "image": {
-            "imageUri": "https://images.news18.com/ibnlive/uploads/2019/05/Google-Maps.jpg?impolicy=website&width=536&height=356",
-            "accessibilityText": "map image"
-          },
-          "buttons": [
-            {
-              "title": "searched",
-              "openUriAction": {
-                "uri": "http://maps.google.co.in"
-              }
-            }
-          ]
-        }
-      },
-    ]
-      response.send(responseJson3);
+    //}
+    //responseJson3.fulfillmentMessages= [
+      //{
+        //"platform": "ACTIONS_ON_GOOGLE",
+        //"simpleResponses": {
+          //"simpleResponses": [
+            //{
+              //"textToSpeech": "You can search for shops near you location here"
+            //}
+          //]
+       // }
+      //},
+      //{
+        //"platform": "ACTIONS_ON_GOOGLE",
+        //"basicCard": {
+          //"title": "90210",
+          //"subtitle": "08-06-2020",
+          //"formattedText": "map",
+          //"image": {
+            //"imageUri": "https://images.news18.com/ibnlive/uploads/2019/05/Google-Maps.jpg?impolicy=website&width=536&height=356",
+            //"accessibilityText": "map image"
+          //},
+          //"buttons": [
+           // {
+             // "title": "searched",
+              //"openUriAction": {
+               // "uri": "http://maps.google.co.in"
+             // }
+           // }
+          //]
+        //}
+      //},
+    //]
+      //response.send(responseJson3);
   
 
-}
-else if(intent == 'searchhistory') {
-    var responseJson4 ={
+//}
+//else if(intent == 'searchhistory') {
+   // var responseJson4 ={
 
-    }
-    logger.info(searchHistory)
-    searchHistory.forEach(element => { 
-        var singleCard={
-   "platform": "ACTIONS_ON_GOOGLE",
+   // }
+    //logger.info(searchHistory)
+    //searchHistory.forEach(element => { 
+       // var singleCard={
+   //"platform": "ACTIONS_ON_GOOGLE",
           
-          "basicCard": {
-            "title": "",
-            "subtitle": "",
-            "formattedText": "",
-            "image": {
-              "imageUri": "",
-              "accessibilityText": ""
-            },
-            "buttons": [
-              {
-                "title": "",
-                "openUriAction": {
-                  "uri": ""
-                }
-              }
-            ]
-          }
-        }
+          //"basicCard": {
+          //  "title": "",
+           // "subtitle": "",
+           // "formattedText": "",
+           // "image": {
+            //  "imageUri": "",
+             // "accessibilityText": ""
+           // },
+           // "buttons": [
+             // {
+              //  "title": "",
+               // "openUriAction": {
+         //         "uri": ""
+             //   }
+        //      }
+       //     ]
+       //   }
+      //  }
       
-        singleCard.basicCard.title = element.Zipcode ;
-        singleCard.basicCard.subtitle = element.date;
-        singleCard.basicCard.formattedText = element.formattedtext;
-        singleCard.basicCard.image.imageUri = "https://images.news18.com/ibnlive/uploads/2019/05/Google-Maps.jpg?impolicy=website&width=536&height=356";
-        singleCard.basicCard.image.accessibilityText = element.accessibilitytext;
-        singleCard.basicCard.buttons[0].title= element.class;
-        singleCard.basicCard.buttons[0].openUriAction.uri= "http://maps.google.co.in"
+      //  singleCard.basicCard.title = element.Zipcode ;
+      //  singleCard.basicCard.subtitle = element.date;
+       // singleCard.basicCard.formattedText = element.formattedtext;
+       // singleCard.basicCard.image.imageUri = "https://images.news18.com/ibnlive/uploads/2019/05/Google-Maps.jpg?impolicy=website&width=536&height=356";
+       // singleCard.basicCard.image.accessibilityText = element.accessibilitytext;
+        //singleCard.basicCard.buttons[0].title= element.class;
+        //singleCard.basicCard.buttons[0].openUriAction.uri= "http://maps.google.co.in"
         
-        cardResponse.push(singleCard);
+        //cardResponse.push(singleCard);
          //final = "searchedzipcode:"+element.Zipcode+"\n"+"searcheddate:"+element.date+"\n"
     
     
-        })
-        responseJson4.fulfillmentMessages = cardResponse;
-        console.log("Response to User", JSON.stringify(responseJson4));
-         response.send(responseJson4);
-   }
-else if(intent =='Default Fallback Intent')
-{
-    var responseJson5={
+       // })
+        //responseJson4.fulfillmentMessages = cardResponse;
+        //console.log("Response to User", JSON.stringify(responseJson4));
+         //response.send(responseJson4);
+   //}
+//else if(intent =='Default Fallback Intent')
+//{
+    //var responseJson5={
 
-    } 
-    responseJson5.fulfillmentMessages= [
-      {
-        "platform": "ACTIONS_ON_GOOGLE",
-        "simpleResponses": {
-          "simpleResponses": [
-            {
-              "textToSpeech": "Enter proper zipcode!"
-            }
-          ]
-        }
-      }
-    ]
-}
+   // } 
+    //responseJson5.fulfillmentMessages= [
+      //{
+       // "platform": "ACTIONS_ON_GOOGLE",
+       // "simpleResponses": {
+         // "simpleResponses": [
+          //  {
+            ///  "textToSpeech": "Enter proper zipcode!"
+          //  }
+       //   ]
+       // }
+     // }
+    //]
+//}
     
 }); 
 app.listen(process.env.PORT || 8080, function () {
